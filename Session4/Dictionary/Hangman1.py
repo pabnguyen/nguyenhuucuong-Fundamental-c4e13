8932006126f538078378974579251c77c7eaaa9a
nhcuongng <33181010+nhcuongng@users.noreply.github.com>
2017-11-08 02:00:50 +0700

    Update Hangman1.py

--- Session4/Dictionary/Hangman1.py
+++ Session4/Dictionary/Hangman1.py
@@ -68,36 +68,34 @@ mylist1 = []
 for i in range (len(list_solution)):
     mylist1.insert(i, '_')
 count = 0
+count1 = 0
 a = True
 while a:
+    print(statues[0])
     your_guess = input("Your word? ")
-
     if your_guess in list_solution:
         print("You Right!")
         for index, item in enumerate(list_solution):
             if list_solution[index] == your_guess:
                 mylist1[index] = your_guess
-        print(*mylist1,sep =" " )
+                count1 +=1
+
+        print(*mylist1,sep = " ")
+        if count1 == len(list_solution):
+            print("You Win")
+            break
     else:
         print("You Wrong")
-        if count == 0:
-            print(statues[0])
-            count += 1
-        elif count == 1:
+        count += 1
+        if count == 1:
             print(statues[1])
-            count += 1
         elif count == 2:
             print(statues[2])
-            count += 1
         elif count == 3:
             print(statues[3])
-            count += 1
         elif count == 4:
             print(statues[4])
-            count+=1
         else:
             print(statues[5])
             print("you lose")
             a = False
-
-print("You Win")

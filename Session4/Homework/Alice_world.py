a05327b8c7bd77e60193058d89e2e0ff682ce440
nhcuongng <33181010+nhcuongng@users.noreply.github.com>
2017-11-09 16:14:36 +0700

    Update Alice_world.py

--- Session4/Homework/Alice_world.py
+++ Session4/Homework/Alice_world.py
@@ -2,26 +2,26 @@
 myf = open('alice.txt', 'r',encoding='utf8')
 
 count = {}
-#read words in lines
+#Đọc từng dòng rồi chia từng dòng thành từng từ
 for line in myf:
     for word in line.split():
 
-        # remove punctuation
+        # xóa những ký tự đặc biệt
         word = word.replace('_', '').replace('"', '').replace(',', '').replace('.', '')
         word = word.replace('-', '').replace('?', '').replace('!', '').replace("'", "")
         word = word.replace('(', '').replace(')', '').replace(':', '').replace('[', '')
         word = word.replace(']', '').replace(';', '')
 
-        # ignore case
+        # chấp nhận ký tự in hoa
         word = word.lower()
 
-        # ignore numbers
+        # chỉ chấp nhận chữ cái
         if word.isalpha():
             if word in count:
                 count[word] = count[word] + 1
             else:
                 count[word] = 1
-
+#xếp theo thứ tự 
 keys = list(count.keys())
 keys.sort()
 
@@ -30,12 +30,13 @@ out = open('alice_words.txt', 'w')
 out.write("Word \t \t \t \tCount\n")
 out.write("====================\n")
 for word in keys:
+    #Tiến hành in key và value
     out.write("%-12s%d\n" % (word, count[word]))
     out.write('\n')
 
 print("The word 'alice' appears " , count['alice'] , " times in the book.")
 
-#lengthest word
+#lTừ Dài nhất
 ks = list(count.keys())
 max_length = len(ks[0])
 for i in range (len(ks)):
